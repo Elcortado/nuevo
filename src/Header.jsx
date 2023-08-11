8963cd2bf69a1740a41414e41ae0c327a9fa06d4
Elcortado <josealejandrobermudez@hotmail.com>
2023-08-10 21:34:26 -0300

    header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <>
+    <div className="w-full bg-slate-200 p-4 flex flex-col md:flex-row xl:flex-row gap-4 items-center justify-center md:justify-around">
+    <h1 className="font-extrabold text-2xl">
+    My Tinerary
+    </h1>
+
+    <NavBar />
+    
+    <button 
+    className=" font-medium flex items-center gap-x-2 border border-black hover:bg-white transition-colors py-2 px-4 rounded flex-col md:flex-row xl:flex-row gap-4 justify-center">
+      <img
+        src="https://img.freepik.com/iconos-gratis/acceso_318-755543.jpg"
+        className="w-5 h-5 object-cover"
+      />
+    Login
+  </button>
+   
+</div>
+
+ 
+</>
   )
 }
